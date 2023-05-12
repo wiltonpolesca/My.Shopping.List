79be25f16175a15afd259e0e0e39c9c26f910eaf
wilton polesca <wiltonpolesca@gmail.com>
2023-05-12 19:02:52 -0400

    Added shopping list id

--- src/My.Shopping.List/Entities/ShoppingListItem.cs
+++ src/My.Shopping.List/Entities/ShoppingListItem.cs
@@ -2,6 +2,7 @@
 
 public class ShoppingListItem
 {
+    public int ShoppingListId { get; set; }
     public string Name { get; set; } = string.Empty;
     public int Qtde { get; set; }
     public double Price { get; set; }
